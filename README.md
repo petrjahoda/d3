5cb711cb7df69581c0aa2488fc55c3b13d9295a7
petrjahoda <petr.jahoda@me.com>
2025-03-06 17:51:30 +0100

    Refactor D3 chart rendering and add README.md
    Refactored the D3 chart rendering logic for improved clarity, scalability, and better use of D3 features like scales and axes. Updated the container styling and added vertical grid lines for better visualization. Introduced a README.md file outlining the D3 chart creation steps.

---
+++ README.md
@@ -0,0 +1,10 @@
+# D3
+
+1. load data using js.fetch
+2. create margins, innerwidth, innerheight
+3. append svg to div with proper width and height
+4. append g as innerchart to svg
+5. create scales from data
+6. create axis from scales, append to svg
+7. format scales using tickformat
+8. create chart from data, append to innerchart
--- js/home.js
+++ js/home.js
@@ -2,7 +2,6 @@ let chartData;
 const container = d3.select("#container")
 
 
-
 fetch("/d3_data", {
     method: "POST", headers: {'Content-Type': 'application/json'}
 }).then(response => response.json().then(data => {
@@ -12,55 +11,85 @@ fetch("/d3_data", {
 })).catch((e) => console.error(e))
 
 function updateChart() {
-    const leftMargin = 150
-    const rightMargin = 50
-    const barHeight = 25
+
+    // create constants for chart
+    const barHeight = 30
+    const margin = {top: 100, right: 0, bottom: 100, left: 80}
+    const innerWidth = container.node().getBoundingClientRect().width
+    const innerHeight = barHeight * chartData["Data"].length - margin.top - margin.bottom
+
+    // clear chart
     container.selectAll("*").remove()
+
+    // append svg to dom
     const svg = container.append("svg")
-                         .attr("width", "100%")
-                         .attr("height", barHeight * chartData["Data"].length)
-                         .attr("top", "10px")
+                         .attr("width", innerWidth)
+                         .attr("height", innerHeight)
+
+    // create chart from constants
+    const chart = svg.append("g")
+                     .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+    // create scales
     const xScale = d3.scaleLinear()
                      .domain([0, d3.max(chartData["Data"], d => d["Age"])])
-                     .range([0, container.node().getBoundingClientRect().width - leftMargin - rightMargin])
+                     .range([0, innerWidth])
+                     .nice()
 
     const yScale = d3.scaleBand()
                      .domain(chartData["Data"].map(d => d["Name"]))
                      .range([0, barHeight * chartData["Data"].length])
                      .paddingInner(0.1)
-    const barAndLabel = svg.selectAll("g")
-                           .data(chartData["Data"])
-                           .join("g")
-                           .attr("transform", d => `translate(0, ${yScale(d["Name"])})`)
 
-    barAndLabel.append("rect")
-               .attr("x", leftMargin)
-               .attr("y", 0)
-               .attr("width", d => xScale(d["Age"]))
-               .attr("height", yScale.bandwidth())
-               .attr("fill", d => d["Color"])
-               .attr("data-name", d => d["Name"])
+    // create axis from scales
+    const xAxis = d3.axisBottom(xScale)
+                    .tickFormat(d => d + " let");
+    const yAxis= d3.axisLeft(yScale)
+                    .tickSize(0)
 
 
-    barAndLabel.append("text")
-               .text(d => d["Name"])
-               .attr("x", leftMargin - 10)
-               .attr("y", barHeight / 2 + 2.5)
-               .attr("text-anchor", "end")
-               .attr("font-family", "sans-serif")
-               .attr("font-size", "12px")
-    barAndLabel.append("text")
-               .text(d => d["Age"])
-               .attr("x", d => xScale(d["Age"]) + leftMargin + 10)
-               .attr("y", barHeight / 2 + 2.5)
-               .attr("font-family", "sans-serif")
-               .attr("font-size", "12px")
-    svg.append("line")
-       .attr("x1", leftMargin)
-       .attr("y1", 0)
-       .attr("x2", leftMargin)
-       .attr("y2", barHeight * chartData["Data"].length)
-       .attr("stroke", "black")
-       .attr("stroke-width", 1);
+
+    chart.append("g")
+         .attr("transform", `translate(0, -30)`)
+         .call(xAxis)
+    chart.append("g")
+         .call(yAxis)
+         .attr("transform", `translate(-0.5, 0)`)
+
+    // append data to chart
+    chart.selectAll(".bar")
+         .data(chartData["Data"])
+         .join("rect")
+         .attr("class", "bar")
+         .attr("x", 0)
+         .attr("y", d => yScale(d["Name"]))
+         .attr("width", d => xScale(d["Age"]))
+         .attr("height", yScale.bandwidth())
+         .attr("fill", d => d["Color"])
+
+    // append labels to data
+    chart.selectAll(".label")
+         .data(chartData["Data"])
+         .join("text")
+         .text(d => d["Age"] + " let")
+         .attr("x", d => xScale(d["Age"]) - 35)
+         .attr("y", d => yScale(d["Name"]) + yScale.bandwidth() / 2)
+         .attr("font-family", "sans-serif")
+         .attr("font-size", "12px")
+         .attr("fill", "white")
+         .attr("alignment-baseline", "middle");
+
+    // Draw vertical grid lines using xScale ticks
+    chart.selectAll(".grid-line")
+              .data(xScale.ticks())
+              .join("line")
+              .attr("class", "grid-line")
+              .attr("x1", d => xScale(d))
+              .attr("y1", 0)
+              .attr("x2", d => xScale(d))
+              .attr("y2", innerHeight)
+              .attr("stroke", "hsla(215, 12%, 15%, 0.2)")
+              .attr("stroke-dasharray", "4,4");
+
+
 }
-
